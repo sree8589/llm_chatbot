87b91b294d4918bce311e65f20f156cbd49a5c04
mahesh <mahesh@expertzlab.com>
2024-08-24 11:53:48 +0530

    new files added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llm_chatbot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "ctransformers==0.2.5",
+        "sentence-transformers==2.2.2",
+        "pinecone-client",
+        "langchain==0.0.27",
+        "flask",
+        "pypdf",
+        "python-dotenv",
+    ],
+)
+
